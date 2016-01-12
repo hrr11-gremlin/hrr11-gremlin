01dc531ccd5552f97281eec242120d182b553562
delventhalz <delventhalz@gmail.com>
2016-01-11 20:25:21 -0600

    (fix) One final group fix.

--- public/group/group.js
+++ public/group/group.js
@@ -3,7 +3,7 @@ angular.module('Recipeoples.group', [])
 .controller('GroupController', function($scope, $rootScope, ProfileFactory){
   $rootScope.focusGroup = $rootScope.focusGroup || {
     name: "Carnivores",
-    members: [{name:'Bear'}, {name:'Tiger'}, {name:'Alligator'}],
+    members: [{username:'Bear'}, {username:'Tiger'}, {username:'Alligator'}],
     recipes: ['Awesome Burger', 'Spicy Tuna Rolls', 'Sloe Gin Fizz']
   };
   $scope.group = $rootScope.focusGroup;
